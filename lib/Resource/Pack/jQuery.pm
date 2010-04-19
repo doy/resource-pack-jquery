0a9198c691bad4c0d074f0674f1baa1f5a060f57
Jesse Luehrs <doy@tozt.net>
2010-04-19 13:49:18 -0500

    initial commit

---
+++ lib/Resource/Pack/jQuery.pm
@@ -0,0 +1,71 @@
+package Resource::Pack::jQuery;
+use Moose;
+
+=head1 NAME
+
+Resource::Pack::jQuery -
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-resource-pack-jquery at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Resource-Pack-jQuery>.
+
+=head1 SEE ALSO
+
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc Resource::Pack::jQuery
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Resource-Pack-jQuery>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Resource-Pack-jQuery>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Resource-Pack-jQuery>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Resource-Pack-jQuery>
+
+=back
+
+=head1 AUTHOR
+
+  Jesse Luehrs <doy at tozt dot net>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Jesse Luehrs.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
+
+1;
